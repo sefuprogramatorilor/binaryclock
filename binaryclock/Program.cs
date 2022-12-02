0d006444b21bb5346dc3af7181034ea17836c384
jeff <jeff@DESKTOP-D0VFHUI>
2022-12-02 16:14:10 +0200

    Add project files.

---
+++ binaryclock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Timer timer = null;
+        timer = new Timer(TimerCallback, null, 0, 1000);
+
+        Console.ReadLine();
+    }
+
+    private static void TimerCallback(object o)
+    {
+        Console.Clear();
+        Console.WriteLine($"Current time: {DateTime.Now.ToLongTimeString()}");
+        Console.WriteLine($"Current time in binary: {Convert.ToString(DateTime.Now.Hour, 2)} : {Convert.ToString(DateTime.Now.Minute, 2)} : {Convert.ToString(DateTime.Now.Second, 2)}");
+        Console.WriteLine("Press any key to stop.");
+
+    }
+}
